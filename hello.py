09de3a79f783b38be423c020bbdd27419a202e1b
Marlon Macedo <marlon.s.macedo@gmail.com>
2023-08-13 17:54:09 -0300

    Implemented Hello World multi language script.

--- hello.py
+++ hello.py
@@ -1,5 +1,36 @@
 #!/usr/bin/env python3
+"""Hello World multi language.
 
-# This program print an Hello World message
+depending on the language configured in the enviroment
+the program displays the corresponding message.
 
-print("Hello World")
+Usage:
+
+Have the enviroment variable properly set, example:
+
+    export LANG=pt_BR
+
+Execution:
+
+    python3 hello.py
+    or
+    ./hello.py
+"""
+__version__ = "0.0.1"
+__authon__ = "Marlon Macedo"
+__licence__ = "Unlicense"
+
+
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+
+msg = "Hello, World!"
+
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+
+print(msg)
