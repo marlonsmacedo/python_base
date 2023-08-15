230e1605f07a94a72bfc829312bee6a34a1addc6
Mmarlon Macedo <marlon.s.macedo@gmail.com>
2023-08-15 14:34:55 -0300

    Add more language (es_SP, fr_FR).

--- hello.py
+++ hello.py
@@ -32,5 +32,9 @@ if current_language == "pt_BR":
     msg = "Olá, Mundo!"
 elif current_language == "it_IT":
     msg = "Ciao, Mondo!"
+elif current_language == "es_SP":
+    msg = "Hola, Mundo!"
+elig current_language == "fr_FR":
+    msg = "Bonjour le monde!"
 
 print(msg)
